e038288352354eae83d0ce4a1c1a076abc6d2f14
durianpool <81219935+durianpool@users.noreply.github.com>
2021-07-11 13:24:13 +0800

    Update replot.ps1

--- replot.ps1
+++ replot.ps1
@@ -1,6 +1,6 @@
 while ($True) {
 	if (Test-Path -Path <OG Plots Directory>\*.plot) {
-		Get-ChildItem <OG Plots Directory> | Sort CreationTime | Select -First 1 | Remove-Item | <Your_MadMax_script.ps1>
+		Get-ChildItem <OG Plots Directory> | Sort CreationTime | Select -First 1 | Remove-Item | <Your_MadMax_chiaplotter.ps1>
 		} else {
 		Break
 		}
